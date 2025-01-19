368f1a494c006d9bcbdcf0615478438a55f88691
hanaarena <hanaarena@gmail.com>
2025-01-19 22:36:13 +0900

    feat(index): add entry list of nav menu of index page & export contents component as forwardRef in order to support call function from Page component(parent->children)

--- app/components/contents.tsx
+++ app/components/contents.tsx
@@ -4,7 +4,7 @@ import { useAtom, useSetAtom } from "jotai";
 import { collapsedAtom, questionTypeAtom } from "./atoms";
 import Dooshi from "./questions/dooshi";
 import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { forwardRef, useEffect, useState, useImperativeHandle } from "react";
 import { findItemByid } from "../utils/const";
 import Moji1 from "./questions/moji-1";
 import Moji2 from "./questions/moji-2";
@@ -12,7 +12,34 @@ import Moji3 from "./questions/moji-3";
 import Bunpoo from "./questions/bunpoo";
 import VerbConjugation from "./questions/verbConjugation";
 
-export default function Contents() {
+const EntryList = [
+  {
+    name: "動詞活用",
+    id: 1
+  },
+  {
+    name: "文法·排序题",
+    id: 2
+  },
+  {
+    name: "漢字",
+    id: 3
+  },
+  {
+    name: "相似词意",
+    id: 4
+  },
+  {
+    name: "最佳选项",
+    id: 5
+  },
+  {
+    name: "动词普通形",
+    id: 6
+  }
+];
+
+const TContents = forwardRef<any>(function Contents({}, ref) {
   const [questionType, setQuestionType] = useAtom(questionTypeAtom);
   const params = useSearchParams();
   const [loading, setLoading] = useState(true);
@@ -21,6 +48,14 @@ export default function Contents() {
   const handleCollapse = () => {
     setCollapsed(true);
   };
+
+  useImperativeHandle(ref, () => {
+    return {
+      handleUpdateQuestionType(id: number) {
+        setQuestionType(id);
+      }
+    };
+  });
 
   useEffect(() => {
     const id = params.get("id");
@@ -32,15 +67,27 @@ export default function Contents() {
   }, [params]);
 
   return (
-    <div className="content-body flex py-2 px-3 bg-white w-full justify-center">
+    <div
+      ref={ref}
+      className="content-body flex py-2 px-3 bg-white w-full justify-center"
+    >
       {!loading &&
         (questionType < 1 ? (
           <div
-            className="flex flex-col w-full justify-center items-center"
+            className="flex w-3/4 justify-center items-center flex-wrap content-center gap-4 underline underline-offset-[6px]"
             onClick={handleCollapse}
           >
-            <div className="text-xl text-black">～ 先选择题目类型 ～</div>
-            {/* todo：random question when enter Landing page */}
+            {EntryList.map((item) => (
+              <div
+                key={item.id}
+                className={`text-lg mb-2 px-2`}
+                onClick={() => {
+                  setQuestionType(item.id);
+                }}
+              >
+                {item.name}
+              </div>
+            ))}
           </div>
         ) : (
           <div
@@ -62,4 +109,6 @@ export default function Contents() {
         ))}
     </div>
   );
-}
+});
+
+export default TContents;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
+
 import { useAtom } from "jotai";
+import { useRef } from "react";
 import Contents from "./components/contents";
 import Sidebar from "./components/sidebar";
 import { collapsedAtom } from "./components/atoms";
@@ -7,6 +9,7 @@ import { cn } from "@/lib/utils";
 
 export default function Home() {
   const [collapsed, setCollapsed] = useAtom(collapsedAtom);
+  const contentRef = useRef<any>(null);
 
   return (
     <main className="flex min-h-screen bg-white">
@@ -16,7 +19,12 @@ export default function Home() {
             "mobile-nav block opacity-0 relative left-3 top-5 cursor-pointer",
             collapsed && "opacity-100"
           )}
-          onTouchStart={() => {
+          // onClick={(e) => {
+          //   e.preventDefault();
+          //   setCollapsed((prev) => !prev);
+          // }}
+          onMouseDown={(e) => {
+            e.preventDefault();
             setCollapsed((prev) => !prev);
           }}
         >
@@ -25,13 +33,18 @@ export default function Home() {
           <div className="w-[20px] h-[3px] bg-black"></div>
         </div>
         <div className="title-wrapper m-auto w-auto">
-          <section className="sweet-title">
+          <section
+            className="sweet-title"
+            onClick={() => {
+              contentRef.current?.handleUpdateQuestionType(-1);
+            }}
+          >
             <span data-text="JLPT EASY!">JLPT EASY!</span>
           </section>
         </div>
       </div>
       <Sidebar />
-      <Contents />
+      <Contents ref={contentRef} />
     </main>
   );
 }
